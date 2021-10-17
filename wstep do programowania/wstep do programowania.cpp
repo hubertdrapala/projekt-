0cb8d2bbc1bd5b0b91d16d2423bb7a482c05d920
hubertdrapala <92692812+hubertdrapala@users.noreply.github.com>
2021-10-17 23:14:53 +0200

    Add files via upload

---
+++ wstep do programowania/wstep do programowania.cpp
@@ -0,0 +1,99 @@
+﻿// wstep do programowania.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //zad1
+
+   /* int numer_albumu;
+    cout << " wprowadz numer albumu\n";
+    cin >> numer_albumu;
+    cout << "w" << numer_albumu << endl;*/
+
+    //zad2
+
+    /*int x;
+    cout << "wprowadz liczbe x: ";
+    cin >> x;
+    if (x % 2 == 0)
+    cout << "podana liczba jest parzysta\n";
+    else
+    cout << "podana liczba jest nieparzysta\n";*/
+
+    //zad3 
+
+   /* float x, y;
+    cout << "wprowadz liczby x i y\n";
+    cout << "wprowadz x:";
+    cin >> x;
+    cout << "wprowadz y:";
+    cin >> y;
+
+    cout << "wieksza liczba to:";
+    if (x >= y)
+    cout << x << endl;
+    else 
+    cout << y << endl;*/
+
+    //zad4
+
+    /*float a, b, c, d, srednia;
+    cout << "Podaj liczbe a: ";
+    cin >> a;
+    cout << "Podaj liczbe b: ";
+    cin >> b;
+    cout << "Podaj liczbe c: ";
+    cin >> c;
+    cout << "Podaj liczbe d: ";
+    cin >> d;
+    srednia = (float)(a + b + c + d) / 4;
+    cout << "Srednia arytmentyczna z podanych liczb wynosi: " << srednia;*/
+
+
+
+    //zad5 
+
+    /*float a, b;
+    int wykonaj_dzialanie;
+
+    cout << "wprowadz a i b \n";
+    cout << "wprowadz a:";
+    cin >> a;
+    cout << "wprowadz b:";
+    cin >> b;
+
+    cout << "\n1. roznica\n";
+    cout << "2. suma\n";
+    cout << "3. iloraz\n";
+    cout << "4. iloczyn\n";
+    cout << "\nwybierz dzialanie:";
+    cin >> wykonaj_dzialanie;
+
+    switch (wykonaj_dzialanie)
+    {
+    case 1:
+        cout << "\nroznica" << a << "-" << b << "=" << a - b << endl;
+        break;
+    case 2:
+        cout << "\nsuma" << a << "+" << b << "=" << a + b << endl;
+        break;
+    case 3:
+        cout << "\niloraz" << a << "/" << b << "=" << a / b << endl;
+        break;
+    case 4:
+        cout << "\niloczyn" << a << "*" << b << "=" << a * b << endl;
+        break;
+    default:
+        cout << "nie mozna wykonac tego dzialania ";
+
+    }*/
+
+
+}
+
+
+
+    
+
